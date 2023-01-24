7407bb87b5eeb2812c2c3914d4b8d493d3e66829
beardfriend <beardfriend21@gmail.com>
2023-01-24 23:46:35 +0900

    작업증명 쪽 파읿 분리

---
+++ 02_poW/block.go
@@ -0,0 +1,31 @@
+package main
+
+import "time"
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+	Nonce         int
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{
+		Timestamp:     time.Now().Unix(),
+		Data:          []byte(data),
+		PrevBlockHash: prevBlockHash,
+		Nonce:         0,
+	}
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
+	return block
+}
+
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
